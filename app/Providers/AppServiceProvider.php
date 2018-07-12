33461daf02cd1ae01cb5f16fb26dfce171ff887e
zibianqu <zibianqu@163.com>
2018-07-12 18:47:31 +0800

    redids 操作

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,11 +29,6 @@ class AppServiceProvider extends ServiceProvider
             $this->app->register(\Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
         }
         
-        //数据库表生成模型，没有成功
-        if ($this->app->environment() == 'local') {
-            $this->app->register('Iber\Generator\ModelGeneratorProvider');
-        }
-        
         //数据库表生成模型
         if ($this->app->environment() == 'local') {
             $this->app->register(\Reliese\Coders\CodersServiceProvider::class);
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Redis;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {  
+        $str='abcdefghijklmnopqrstuvwxwz0123456789';
+        for($i=0;$i<5;$i++)
+        {
+            Redis::set('test1',$str[rand(0,strlen($str)-1)]);
+        }
+        var_export(Redis::del('test1'));
+        $this->assertTrue(true);
+    }
+}
